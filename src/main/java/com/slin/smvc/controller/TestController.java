e7074d9074e6de5cc1555cb0648fdeb193409ec2
f1008611 <andyadny1988@gmail.com>
2014-04-17 15:11:43 +0800

    commit

--- src/main/java/com/slin/smvc/controller/TestController.java
+++ src/main/java/com/slin/smvc/controller/TestController.java
@@ -17,6 +17,7 @@ public class TestController {
     public String test(HttpServletRequest request,Model model){
         System.out.println("come in here ..... ");
         request.setAttribute("userList","userList");
+
         return "/user/userList";
     }
 }
