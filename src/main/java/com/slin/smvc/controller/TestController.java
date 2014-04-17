5fbce70936bf3fbe8f109d974e95773ebb9cefb4
f1008611 <andyadny1988@gmail.com>
2014-04-17 14:53:24 +0800

    commit

---
+++ src/main/java/com/slin/smvc/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.slin.smvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 14-4-17.
+ */
+@Controller
+@RequestMapping("/user")
+public class TestController {
+
+    @RequestMapping("/userList")
+    public String test(HttpServletRequest request,Model model){
+        System.out.println("come in here ..... ");
+        return "/user/userList";
+    }
+}
