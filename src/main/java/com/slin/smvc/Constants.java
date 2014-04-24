55948291940e4580b0565d70866493f69fc85af7
f1008611 <andyadny1988@gmail.com>
2014-04-24 11:27:21 +0800

    commit

--- src/main/java/com/slin/smvc/Constants.java
+++ src/main/java/com/slin/smvc/Constants.java
@@ -1,5 +1,6 @@
 package com.slin.smvc;
 
 public class Constants {
-	public static final String NS="http://service.server.cxf.webservice/";
+	//public static final String NS="http://service.server.cxf.webservice/";
+	public static final String NS="http://com.slin.smvc.service/";
 }
