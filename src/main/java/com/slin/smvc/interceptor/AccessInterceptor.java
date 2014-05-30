aba4503e746f99e4ea0dfe0f7fc267fbfbf776fc
f1008611 <andyadny1988@gmail.com>
2014-05-30 10:30:40 +0800

    commit

---
+++ src/main/java/com/slin/smvc/interceptor/AccessInterceptor.java
@@ -0,0 +1,17 @@
+package com.slin.smvc.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 14-5-30.
+ */
+public class AccessInterceptor  extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return super.preHandle(request, response, handler);
+    }
+}
