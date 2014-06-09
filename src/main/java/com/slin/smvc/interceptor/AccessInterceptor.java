dd9ba4f27c53b8af52ae3514af2aee02814a5b10
f1008611 <andyadny1988@gmail.com>
2014-06-09 17:54:06 +0800

    commit

--- src/main/java/com/slin/smvc/interceptor/AccessInterceptor.java
+++ src/main/java/com/slin/smvc/interceptor/AccessInterceptor.java
@@ -1,17 +1,51 @@
 package com.slin.smvc.interceptor;
 
+import com.slin.smvc.pojo.SmvcUser;
+import com.slin.smvc.service.SmvcPrivilegeService;
+import com.slin.smvc.service.SmvcUserService;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
 
 /**
  * Created by Administrator on 14-5-30.
  */
 public class AccessInterceptor  extends HandlerInterceptorAdapter {
+    private SmvcUserService smvcUserService;
+    private SmvcPrivilegeService smvcPrivilegeService;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("before  come in.............");
+        String requestUrl=request.getServletPath();
+        if("/admin/login.do".equals(requestUrl)){
+            if(!smvcUserService.isLogin(request.getSession())){
+                return false;
+            }else{
+                SmvcUser smvcUser=smvcUserService.getCurrentCmsUser(request.getSession());
+                if(smvcUser!=null&&smvcPrivilegeService.isPassReqUrl(request.getSession(),requestUrl)){
+                    //check privileges codes
+                } else {
+                    return false;
+                }
+            }
+        }else {
+            if(!smvcUserService.isLogin(request.getSession())){
+                return  false;
+            }else{
+                //check privileges
+                SmvcUser smvcUser= smvcUserService.getCurrentCmsUser(request.getSession());
+                if(smvcUser!=null&&smvcPrivilegeService.isPassReqUrl(request.getSession(),requestUrl)){
+
+                }else {
+                    return false;
+                }
+
+            }
+            request.setAttribute("loginErrorMessage","该用户没权限。。。。");
+        }
+
         return super.preHandle(request, response, handler);
     }
 }
